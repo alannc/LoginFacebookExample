e36a5260f8534f9e003b319d68b28a9bc3de6bb5
Alan Castro <alannnc@gmail.com>
2017-09-14 19:08:31 -0500

    Added facebook sdk

---
+++ app/src/main/java/com/alannnc/logintecmilenio/logintecmilenio/LoginActivty.java
@@ -0,0 +1,15 @@
+package com.alannnc.logintecmilenio.logintecmilenio;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import com.facebook.FacebookSdk;
+import com.facebook.appevents.AppEventsLogger;
+
+public class LoginActivty extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
